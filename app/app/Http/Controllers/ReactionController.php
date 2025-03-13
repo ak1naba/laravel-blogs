8709675729e7137d42d0b475693c1e67eae6e07f
sergey kondratev <sergejkondratev985@gmail.com>
2025-03-13 15:51:46 +0500

    add depend for reverb

---
+++ app/app/Http/Controllers/ReactionController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class ReactionController extends Controller
+{
+    public function index()
+    {
+        return view('pages.reaction.index');
+    }
+}
--- app/routes/web.php
+++ app/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\ReactionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/reaction', [ReactionController::class, 'index'])->name('reaction.index');
