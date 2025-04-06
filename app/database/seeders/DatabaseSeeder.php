d6a7f37083155890ce40c2087edb12eecbfc24a6
Sergey Kondratev <sergejkondratev985@gmail.com>
2025-04-06 20:21:40 +0500

    add adminer phpmyadmin

--- app/database/seeders/DatabaseSeeder.php
+++ app/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\NewItem;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -20,5 +21,50 @@ class DatabaseSeeder extends Seeder
             'name' => 'admin',
             'role' => 'admin'
         ]);
+
+        // Писатели
+        $writer1 = User::create([
+            'email' => 'writer1@mail.ru',
+            'password' => Hash::make('qweqweqwe'),
+            'name' => 'Writer One',
+            'role' => 'writer'
+        ]);
+
+        $writer2 = User::create([
+            'email' => 'writer2@mail.ru',
+            'password' => Hash::make('qweqweqwe'),
+            'name' => 'Writer Two',
+            'role' => 'writer'
+        ]);
+
+        // Пользователи
+        $users = collect();
+        for ($i = 1; $i <= 10; $i++) {
+            $user = User::create([
+                'email' => "user{$i}@mail.ru",
+                'password' => Hash::make('qweqweqwe'),
+                'name' => "User {$i}",
+                'role' => 'user'
+            ]);
+            $users->push($user);
+        }
+
+        for ($i = 1; $i <= 10; $i++) {
+            NewItem::create([
+                'user_id' => $writer1->id,
+                'title' => "Новость #{$i}",
+                'text' => "Текст новости номер {$i}.",
+                'published' => (bool) rand(0, 1),
+            ]);
+        }
+
+        for ($i = 11; $i <= 20; $i++) {
+            NewItem::create([
+                'user_id' => $writer2->id,
+                'title' => "Новость #{$i}",
+                'text' => "Текст новости номер {$i}.",
+                'published' => (bool) rand(0, 1),
+            ]);
+        }
     }
 }
